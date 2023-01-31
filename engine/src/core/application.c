6c064dc1f7b3911efc4ffa55ba47679de91379aa
zhanzhan <270428890@qq.com>
2023-01-31 21:44:18 +0800

    vulkan initialize

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -7,6 +7,9 @@
 #include "core/kmemory.h"
 #include "core/event.h"
 #include "core/input.h"
+#include "core/clock.h"
+
+#include "renderer/renderer_frontend.h"
 
 typedef struct application_state
 {
@@ -16,6 +19,7 @@ typedef struct application_state
     platform_state platform;
     i16 width;
     i16 height;
+    clock clock;
     f64 last_time;
 } application_state;
 
@@ -72,6 +76,13 @@ b8 application_create(game* game_inst)
         return FALSE;
     }
 
+    // Renderer startup
+    if(!renderer_initialize(game_inst->app_config.name, &app_state.platform))
+    {
+        KFATAL("Failed to initialize renderer. Aborting application.");
+        return FALSE;
+    }
+
     // Initialize the game
     if(!app_state.game_inst->initialize(app_state.game_inst))
     {
@@ -88,6 +99,13 @@ b8 application_create(game* game_inst)
 
 b8 application_run()
 {
+    clock_start(&app_state.clock);
+    clock_update(&app_state.clock);
+    app_state.last_time = app_state.clock.elapsed;
+    f64 runing_time = 0;
+    u8 frame_count = 0;
+    f64 target_frame_seconds = 1.0f / 60;
+
     KINFO(get_memory_usage_str());
 
     while(app_state.is_running)
@@ -99,7 +117,13 @@ b8 application_run()
 
         if(!app_state.is_suspended)
         {
-            if(!app_state.game_inst->update(app_state.game_inst, (f32)0))
+            // Update clock and get delta time
+            clock_update(&app_state.clock);
+            f64 current_time = app_state.clock.elapsed;
+            f64 delta = (current_time - app_state.last_time);
+            f64 frame_start_time = platform_get_absolute_time();
+
+            if(!app_state.game_inst->update(app_state.game_inst, (f32)delta))
             {
                 KFATAL("Game update failed, shutting down.");
                 app_state.is_running = FALSE;
@@ -107,18 +131,46 @@ b8 application_run()
             }
 
             // Call the game's render routine
-            if(!app_state.game_inst->render(app_state.game_inst, (f32)0))
+            if(!app_state.game_inst->render(app_state.game_inst, (f32)delta))
             {
                 KFATAL("Game update failed, shutting down.");
                 app_state.is_running = FALSE;
                 break;
             }
 
+            // TODO: refactor packet creation
+            render_packet packet;
+            packet.delta_time = delta;
+            renderer_draw_frame(&packet);
+
+            // Figure out how long the frame took and, if below
+            f64 frame_end_time = platform_get_absolute_time();
+            f64 frame_elapsed_time = frame_end_time - frame_start_time;
+            runing_time += frame_elapsed_time;
+            f64 remaining_seconds = target_frame_seconds - frame_elapsed_time;
+
+            if(remaining_seconds > 0)
+            {
+                u64 remaining_ms = (remaining_seconds * 1000);
+
+                // If there is time left, give it back to the OS
+                b8 limit_frames = FALSE;
+                if(remaining_ms > 0 && limit_frames)
+                {
+                    platform_sleep(remaining_ms - 1);
+                }
+
+                frame_count++;
+            }
+
             // NOTE: Input update/state copying should always be handled
             // after any input should be recorded; I.E. before this line.
             // As a safety, input is the last thing to be updated before
             // this frame ends.
-            input_update(0);
+            input_update(delta);
+
+            // Update last time
+            app_state.last_time = current_time;
         }
     }
 
@@ -131,6 +183,8 @@ b8 application_run()
 
     event_shutdown();
     input_shutdown();
+
+    renderer_shutdown();
 
     platform_shutdown(&app_state.platform);
 
